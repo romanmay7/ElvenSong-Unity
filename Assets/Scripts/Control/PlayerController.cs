c60c2e51a40a61ef2f815c8856182311da83ec43
RomanMayer7 <romanmayerzon@gmail.com>
2020-06-29 20:42:11 +0300

    Adding the Priorities for Actions

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -11,14 +11,15 @@ namespace ElvenSong.Control
         //bool isMoving;
         private void Update()
         {
-            InteractWithCombat();
-            InteractWithMovement();
+            if (InteractWithCombat()) return; //If there are Combat Requests, skip the Movement
+            if (InteractWithMovement()) { print("Move!"); return; } //If Movement is possible perform it and return
+            print("Nothere to Go");
 
             // GetComponent<Mover>().MoveByKeyboard();
 
         }
 
-        private void InteractWithCombat()
+        private bool InteractWithCombat() //Check if Mouse Click Events are for CombatTargets to call Attack(target)
         {
 
         //Returns a List of all the things it hits
@@ -30,27 +31,31 @@ namespace ElvenSong.Control
                 if (Input.GetMouseButtonDown(0))
                 {
                     GetComponent<Fighter>().Attack(target);
+                    
                 }
+                return true;
             }
+            return false;
         }
 
-        private void InteractWithMovement()
+        private bool InteractWithMovement() //Check for Mouse Events to call  MoveToCursor() method
         {
-            if (Input.GetMouseButton(0))
+            RaycastHit hit; //Storing info(position) about where the ray cast will  hit inside this variable
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (hasHit) //If We are hovering over something within range,available to move there
             {
-                MoveToCursor();
+                if (Input.GetMouseButton(0))//And if we also  have Click Event in the same time period,then we can Move
+                {
+                    MoveToCursor(hit.point);
+                }
+                return true;
             }
+            return false; //we are out of the range
         }
 
-        private void MoveToCursor()
+        private void MoveToCursor(Vector3 point)
         {
-          
-            RaycastHit hit;//Storing info(position) about where the ray cast will  hit inside this variable
-            bool hasHit = Physics.Raycast( GetMouseRay(), out hit);
-            if (hasHit)
-            {
-                GetComponent<Mover>().MoveTo(hit.point);
-            }
+            GetComponent<Mover>().MoveTo(point);
         }
 
         private static Ray GetMouseRay()
